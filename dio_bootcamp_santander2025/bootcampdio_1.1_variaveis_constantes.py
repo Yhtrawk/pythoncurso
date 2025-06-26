27254959b546a049e0989d5063776cac4756a4ea
unknown <luizg.s.m.36@gmail.com>
2025-06-26 17:46:48 -0300

    Commit

---
+++ dio_bootcamp_santander2025/bootcampdio_1.1_variaveis_constantes.py
@@ -0,0 +1,16 @@
+nome = 'Luiz Gustavo'
+idade = 26
+
+nome = 'Fernando'
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+limiteSaqueDiario = 1000
+lim_saq_di = 1000
+
+# Variáveis em caps lock, por convenção, são tratadas como constantes
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'RS']
+
+print(BRAZILIAN_STATES)
